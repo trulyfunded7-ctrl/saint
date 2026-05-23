09586ea37384f8b5d6563b0c9a574460d5212385
trulyfunded7-ctrl <trulyfunded7@gmail.com>
2026-05-23 18:45:11 -0400

    Update bot.js

--- src/config/bot.js
+++ src/config/bot.js
@@ -25,7 +25,7 @@ export const botConfig = {
     activities: [
       {
         // Text users will see (example: "Playing /help | Titan Bot").
-        name: "hi chat",
+        name: "love you all!!",
         // Activity type number (0 = Playing).
         type: 1, 
       },
